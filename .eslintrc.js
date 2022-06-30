ce0381d9e18d8851d52377afcdb3de05aa6cf2c6
Anastasiya <84740334+AnastasiyaMAV@users.noreply.github.com>
2022-06-30 16:00:52 +0200

    project upload

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+      browser: true,
+    },
+    extends: ['airbnb', 'airbnb/hooks', 'react-app', 'prettier', 'prettier/react'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: ['prettier', 'react', '@typescript-eslint'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-console': 'error',
+      'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx', '.ts', '.js'] }],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          ts: 'never',
+          tsx: 'never',
+          js: 'never',
+          jsx: 'never',
+        },
+      ],
+    },
+  };
